73b18a6434b69787f4acc82ccbcfdc10653a61a5
jeremyakd <jeremiasc@exo.com.ar>
2020-05-03 20:50:38 -0300

    CLASE 02/05

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True)
-    email = forms.EmailField(label="Email", required=True)
-    content = forms.CharField(label="Contenido", required=True, widget=forms.Textarea)
+    name = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder':'Escribe tu nombre por favor.',
+        }
+    ), min_length=2, max_length=100)
+    email = forms.EmailField(label="Email", required=True, widget=forms.EmailInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder':'Escribe tu email por favor.',
+        }
+    ), min_length=2, max_length=100)
+    content = forms.CharField(label="Contenido", required=True, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder':'Escribe tu comentario por favor.',
+            'rows': 5
+        }
+    ), min_length=2, max_length=100)
--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.urls import reverse
 
-# Create your views here.
 
 def contact(request):
     contact_form = ContactForm()
+    
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name = request.POST.get('name', '')
+            email = request.POST.get('email', '')
+            content = request.POST.get('content', '')
+        return redirect(reverse('contact')+'?ok')
+            
     return render(request, "contact/contact.html",{'formulario':contact_form})
 
