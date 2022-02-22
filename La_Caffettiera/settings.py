fb28f0f50ab954c19c4b9fd03af57a5c0595faaa
Jeremias <49762948+jeremyakd@users.noreply.github.com>
2022-02-22 17:59:54 -0300

    Update settings.py

--- La_Caffettiera/settings.py
+++ La_Caffettiera/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Que mapee los archivos estaticos
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
